707620b62a99bf2c0571c6ed7797ba32136d6600
MarsMyth <callumshelton2503@gmail.com>
2024-09-03 17:11:47 +1000

    All Datagen Updated

--- src/main/java/net/mars_myth/end_remastered/TheEndRemasteredDataGenerator.java
+++ src/main/java/net/mars_myth/end_remastered/TheEndRemasteredDataGenerator.java
@@ -2,10 +2,18 @@ package net.mars_myth.end_remastered;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.mars_myth.end_remastered.datagen.*;
 
 public class TheEndRemasteredDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModRecipeGenerator::new);
+		pack.addProvider(ModLootTableGenerator::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModAdvancementGenerator::new);
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
 	}
 }
--- src/main/java/net/mars_myth/end_remastered/datagen/ModLootTableGenerator.java
+++ src/main/java/net/mars_myth/end_remastered/datagen/ModLootTableGenerator.java
@@ -1,4 +1,18 @@
 package net.mars_myth.end_remastered.datagen;
 
-public class ModLootTableGenerator {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModLootTableGenerator extends FabricBlockLootTableProvider {
+    public ModLootTableGenerator(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+
+    }
 }
