1959238160ec5133da109242f93592ccaae81dbb
MarsMyth <callumshelton2503@gmail.com>
2024-09-03 16:56:42 +1000

    BlockTagProvider Added

--- src/main/java/net/mars_myth/end_remastered/datagen/ModBlockTagProvider.java
+++ src/main/java/net/mars_myth/end_remastered/datagen/ModBlockTagProvider.java
@@ -1,18 +1,4 @@
 package net.mars_myth.end_remastered.datagen;
 
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.registry.RegistryWrapper;
-
-import java.util.concurrent.CompletableFuture;
-
-public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
-    public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-        super(output, registriesFuture);
-    }
-
-    @Override
-    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-
-    }
+public class ModBlockTagProvider {
 }
