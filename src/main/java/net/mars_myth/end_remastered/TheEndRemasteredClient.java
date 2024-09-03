a620f062ca83745683ea972d28baa9e3fb3a0afb
MarsMyth <callumshelton2503@gmail.com>
2024-09-03 16:42:09 +1000

    Original Commit

---
+++ src/main/java/net/mars_myth/end_remastered/TheEndRemasteredClient.java
@@ -0,0 +1,10 @@
+package net.mars_myth.end_remastered;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheEndRemasteredClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
