5ae14c26b731efab0c5d5c4f85c1fdd3df552d77
MarsMyth <callumshelton2503@gmail.com>
2024-09-03 16:43:07 +1000

    Very Important

--- src/main/java/net/mars_myth/end_remastered/TheEndRemastered.java
+++ src/main/java/net/mars_myth/end_remastered/TheEndRemastered.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// This is the main Class
 public class TheEndRemastered implements ModInitializer {
 	public static final String MOD_ID = "end-remastered";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
