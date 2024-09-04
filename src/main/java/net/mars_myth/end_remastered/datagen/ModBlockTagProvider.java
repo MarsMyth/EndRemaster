44afbd8755940feefe9ec90afcb9d9fd6e7474ab
MarsMyth <callumshelton2503@gmail.com>
2024-09-04 18:58:28 +1000

    Added Block Tags

--- src/main/java/net/mars_myth/end_remastered/datagen/ModBlockTagProvider.java
+++ src/main/java/net/mars_myth/end_remastered/datagen/ModBlockTagProvider.java
@@ -2,7 +2,9 @@ package net.mars_myth.end_remastered.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.mars_myth.end_remastered.block.ModBlocks;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.BlockTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -13,6 +15,25 @@ public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
 
     @Override
     public void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+
+        getOrCreateTagBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.END_METAL_ORE)
+                .add(ModBlocks.END_METAL_BLOCK)
+                .add(ModBlocks.ENDER_LAMP)
+                .add(ModBlocks.END_MULCH_BRICKS)
+                .add(ModBlocks.END_MULCH)
+        ;
+
+        getOrCreateTagBuilder(BlockTags.PICKAXE_MINEABLE)
+                .add(ModBlocks.END_METAL_BLOCK)
+                .add(ModBlocks.END_METAL_ORE)
+                .add(ModBlocks.ENDER_LAMP)
+                .add(ModBlocks.END_MULCH_BRICKS)
+        ;
+
+        getOrCreateTagBuilder(BlockTags.SHOVEL_MINEABLE)
+                .add(ModBlocks.END_MULCH)
+        ;
 
     }
 }
